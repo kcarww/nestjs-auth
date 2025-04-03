6d7da4b4b4cc026ac3f73bad18732a43f64cebc5
kcarww <carlos.oe.cc@gmail.com>
2025-04-03 16:02:03 -0300

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: {
+        ...createUserDto,
+        password: bcrypt.hashSync(createUserDto.password, 10),
+      },
+    });
+  }
+
+  findAll() {
+    return this.prismaService.user.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prismaService.user.update({
+      data: updateUserDto,
+      where: { id },
+    });
+  }
+
+  remove(id: string) {
+    return this.prismaService.user.delete({
+      where: { id },
+    });
+  }
+}
